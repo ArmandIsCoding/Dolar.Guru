3eb2ab2fb0ad9f4a6c5d9dcfe2469f040940ebdc
Armando Andrés Meabe <armando.meabe@outlook.com>
2025-05-24 03:45:19 -0300

    Chart not working

---
+++ Blazor/ARM.Dolar.Guru/ARM.Dolar.Guru/BLL/DolarHistoricoClient.cs
@@ -0,0 +1,32 @@
+﻿using ARM.Dolar.Guru.DTO;
+using System.Text.Json;
+
+namespace ARM.Dolar.Guru.BLL
+{
+    public static class DolarHistoricoClient
+    {
+        private static readonly HttpClient _httpClient = new HttpClient();
+
+        public static async Task<List<DolarHistorico>> ObtenerDolarHistoricoAsync(string endpoint)
+        {
+            try
+            {
+                HttpResponseMessage respuesta = await _httpClient.GetAsync(endpoint);
+                respuesta.EnsureSuccessStatusCode();
+
+                string contenidoJson = await respuesta.Content.ReadAsStringAsync();
+                var cotizaciones = JsonSerializer.Deserialize<List<DolarHistorico>>(contenidoJson, new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                });
+
+                return cotizaciones ?? new List<DolarHistorico>();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error al obtener DolarHistorico: {ex.Message}");
+                return new List<DolarHistorico>();
+            }
+        }
+    }
+}
---
+++ Blazor/ARM.Dolar.Guru/ARM.Dolar.Guru/DTO/DolarHistorico.cs
@@ -0,0 +1,10 @@
+﻿namespace ARM.Dolar.Guru.DTO
+{
+    public class DolarHistorico
+    {
+        public string Casa { get; set; }
+        public decimal? Compra { get; set; }
+        public decimal? Venta { get; set; }
+        public DateTime? Fecha { get; set; }
+    }
+}
